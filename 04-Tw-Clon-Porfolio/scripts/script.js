2fa0d4a79bc884b550c6ddd756ec75a3566cc24f
NoelSamaDEV <eloypargadev@gmail.com>
2024-04-22 17:22:51 +0200

    Se crea el apartado Proyectos y Experiencia

--- 04-Tw-Clon-Porfolio/scripts/script.js
+++ 04-Tw-Clon-Porfolio/scripts/script.js
@@ -1,9 +1,40 @@
-function cambiarEstilo() {
-    let estiloActual = document.getElementById('estiloHoja').getAttribute('href');
-    
-    if (estiloActual === '../styles/dia.css') {
-        document.getElementById('estiloHoja').setAttribute('href', '../styles/noche.css');
-    } else {
-        document.getElementById('estiloHoja').setAttribute('href', '../styles/dia.css');
-    }
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll(".menu-container ul li a");
+    const sections = document.querySelectorAll("section");
+
+    links.forEach(link => {
+        link.addEventListener("click", function(e) {
+            e.preventDefault();
+
+            const targetId = e.target.getAttribute('href').replace('#', '') + "-container";
+            sections.forEach(section => {
+                if(section.classList.contains(targetId)) {
+                    section.style.display = "block"; // Muestra el section correspondiente
+                    section
+                } else {
+                    section.style.display = "none"; // Oculta los otros sections
+                }
+            });
+        });
+    });
+});
+
+function setupInbox() {
+    // Selecciona todos los elementos li dentro del contenedor 'menu-container'
+    const listItems = document.querySelectorAll(".menu-container li");
+
+    // Añade un escuchador de eventos a cada li
+    listItems.forEach(li => {
+        li.addEventListener("click", function() {
+            // Busca si algún elemento ya tiene el ID 'inbox' y remuévelo
+            const previousInbox = document.getElementById("inbox");
+            if (previousInbox) {
+                previousInbox.removeAttribute("id");
+            }
+            // Establece el ID 'inbox' al li clickeado
+            li.id = "inbox";
+        });
+    });
 }
+
+document.addEventListener("DOMContentLoaded", setupInbox);
